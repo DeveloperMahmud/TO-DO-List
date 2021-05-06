9c228d94b9c10e9c2620ec93b36806d9a1767dde
prince <mrepte700@gmail.com>
2021-05-07 01:26:13 +0600

    converted ES5 prototype to ES6 Class

--- UI.js
+++ UI.js
@@ -1,118 +1,116 @@
 import LS from './LS.js'
 
-function UI(){
-
-};
-
 const ls = new LS();
-
-UI.prototype.addToUI = function(task){
-    ls.storeTask(task);
-    let newHtml = `
-    <div class="task" data-createdat="${task.id}">
-        <div class="task__details">
-            <input type="checkbox" class="task-check" />
-            <label class="task-title">${task.title}</label>
-        </div>
-
-        <div class="task__op">
-            <ion-icon class="task__op_edit" name="create-outline"></ion-icon>
-            <ion-icon class="task__op_delete" name="trash-outline"></ion-icon>
-        </div>
-    </div>
-    `;
-
-    document.querySelector('.task-list').insertAdjacentHTML('afterbegin', newHtml);
-
-};
-
-UI.prototype.resetForm = function(){
-    document.querySelector('#newtaskID').value = '';
-};
-
-UI.prototype.deleteTask = function(e){
-    const task =  e.target.parentElement.parentElement;
-    const id = (task.dataset.createdat);
-    ls.deleteTask(id);
-    task.remove();
-}
-
-UI.prototype.completedTask = function(e){
-    const task =  e.target.parentElement.parentElement;
-    const id = (task.dataset.createdat);
-    ls.completeTask(id);
-    task.classList.toggle('completed');
-}
-
-UI.prototype.showAllTasks = function(){
-    let tasks = ls.fetchTask();
-    let newHtml = '';
-    tasks.forEach((task) => {
-        newHtml += `
-            <div class="task ${task.isCompleted ? 'completed' : ''}" data-createdat="${task.id}">
+class UI {
+        addToUI (task){
+            ls.storeTask(task);
+            let newHtml = `
+            <div class="task" data-createdat="${task.id}">
                 <div class="task__details">
-                    <input type="checkbox" class="task-check  ${task.isCompleted ? 'checked' : ''}" />
+                    <input type="checkbox" class="task-check" />
                     <label class="task-title">${task.title}</label>
                 </div>
-
+        
                 <div class="task__op">
                     <ion-icon class="task__op_edit" name="create-outline"></ion-icon>
                     <ion-icon class="task__op_delete" name="trash-outline"></ion-icon>
                 </div>
             </div>
-        `;
-    })
-
-    document.querySelector('.task-list').innerHTML = newHtml;
-
-}
-
-UI.prototype.editTask = function(e){
-    const task = e.target.parentElement.parentElement;
-    const id = task.dataset.createdat;
-    const data = ls.findTask(id);
-    document.querySelector('#newtaskID').value = data.title;
-    document.querySelector('#updateTaskId').value = data.id;
-
-    document.querySelector('.AddTaskBtn').style.display = 'none';
-    document.querySelector('.EditTaskBtn').style.display = 'inline';
-    document.querySelector('.CancelTaskBtn').style.display = 'inline';
-
-}
-
-UI.prototype.updateTask = function(e){
-    const taskId = document.querySelector('#updateTaskId').value;
-    const taskTitle = document.querySelector('#newtaskID').value;
-    const tasks = document.querySelectorAll('.task-title');
-
-    if(taskTitle.length > 0 ){
-        ls.updateTask(taskId, taskTitle);
-        tasks.forEach((title) => {
-            
-            if(title.parentElement.parentElement.dataset.createdat === taskId){
-                title.innerText = taskTitle;
+            `;
+        
+            document.querySelector('.task-list').insertAdjacentHTML('afterbegin', newHtml);
+        
+        };
+        
+        resetForm (){
+            document.querySelector('#newtaskID').value = '';
+        };
+        
+        deleteTask (e){
+            const task =  e.target.parentElement.parentElement;
+            const id = (task.dataset.createdat);
+            ls.deleteTask(id);
+            task.remove();
+        }
+        
+        completedTask (e){
+            const task =  e.target.parentElement.parentElement;
+            const id = (task.dataset.createdat);
+            ls.completeTask(id);
+            task.classList.toggle('completed');
+        }
+        
+        showAllTasks (){
+            let tasks = ls.fetchTask();
+            let newHtml = '';
+            tasks.forEach((task) => {
+                newHtml += `
+                    <div class="task ${task.isCompleted ? 'completed' : ''}" data-createdat="${task.id}">
+                        <div class="task__details">
+                            <input type="checkbox" class="task-check  ${task.isCompleted ? 'checked' : ''}" />
+                            <label class="task-title">${task.title}</label>
+                        </div>
+        
+                        <div class="task__op">
+                            <ion-icon class="task__op_edit" name="create-outline"></ion-icon>
+                            <ion-icon class="task__op_delete" name="trash-outline"></ion-icon>
+                        </div>
+                    </div>
+                `;
+            })
+        
+            document.querySelector('.task-list').innerHTML = newHtml;
+        
+        }
+        
+        editTask (e){
+            const task = e.target.parentElement.parentElement;
+            const id = task.dataset.createdat;
+            const data = ls.findTask(id);
+            document.querySelector('#newtaskID').value = data.title;
+            document.querySelector('#updateTaskId').value = data.id;
+        
+            document.querySelector('.AddTaskBtn').style.display = 'none';
+            document.querySelector('.EditTaskBtn').style.display = 'inline';
+            document.querySelector('.CancelTaskBtn').style.display = 'inline';
+        
+        }
+        
+        updateTask (e){
+            const taskId = document.querySelector('#updateTaskId').value;
+            const taskTitle = document.querySelector('#newtaskID').value;
+            const tasks = document.querySelectorAll('.task-title');
+        
+            if(taskTitle.length > 0 ){
+                ls.updateTask(taskId, taskTitle);
+                tasks.forEach((title) => {
+                    
+                    if(title.parentElement.parentElement.dataset.createdat === taskId){
+                        title.innerText = taskTitle;
+                    };
+                });
             };
-        });
-    };
- 
-    document.querySelector('#newtaskID').value = '';
-    document.querySelector('#updateTaskId').value = '';
+         
+            document.querySelector('#newtaskID').value = '';
+            document.querySelector('#updateTaskId').value = '';
+        
+            document.querySelector('.AddTaskBtn').style.display = 'inline';
+            document.querySelector('.EditTaskBtn').style.display = 'none';
+            document.querySelector('.CancelTaskBtn').style.display = 'none';
+        
+        };
+        
+        cancelTask (e){
+            
+            document.querySelector('#newtaskID').value = '';
+            document.querySelector('#updateTaskId').value = '';
+        
+            document.querySelector('.AddTaskBtn').style.display = 'inline';
+            document.querySelector('.EditTaskBtn').style.display = 'none';
+            document.querySelector('.CancelTaskBtn').style.display = 'none';
+        };
 
-    document.querySelector('.AddTaskBtn').style.display = 'inline';
-    document.querySelector('.EditTaskBtn').style.display = 'none';
-    document.querySelector('.CancelTaskBtn').style.display = 'none';
-
-};
-
-UI.prototype.cancelTask = function(e){
-    
-    document.querySelector('#newtaskID').value = '';
-    document.querySelector('#updateTaskId').value = '';
-
-    document.querySelector('.AddTaskBtn').style.display = 'inline';
-    document.querySelector('.EditTaskBtn').style.display = 'none';
-    document.querySelector('.CancelTaskBtn').style.display = 'none';
-};
+}
 
 
 export default UI;
